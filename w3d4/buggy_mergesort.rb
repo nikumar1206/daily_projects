323828e44f67d561db60fb511600d247dbc9c37e
Adam Liang <ayl292@nyu.edu>
2019-10-10 10:19:24 -0400

    Add w3d4

---
+++ w3d4/buggy_mergesort.rb
@@ -0,0 +1,27 @@
+  class Array
+    def merge_sort(&prc) # Procifies the block
+      return self if arr.count <= 1
+
+      middle = self.length / 2
+      left = self[0...middle]
+      right = self[middle..-1]
+
+      sorted_left = merge_sort(left)
+      sorted_right = merge_sort(right)
+      merge(sorted_left, sorted_right, prc)
+    end
+
+    private
+    def self.merge(left, right, prc)
+      result = []
+      until left.empty? || right.empty?
+        if prc.call(left[0], right[0]) <= 0
+          result += left.shift
+        else
+          result += right.shift
+        end
+      end
+
+      result + left + right
+    end
+  end
